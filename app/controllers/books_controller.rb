cb09fb5ba4aedb6e6d07cdb2ff1fbeb6d8354535
EC2 Default User <ec2-user@ip-172-31-14-197.ec2.internal>
2022-02-20 11:09:00 +0000

    [Finish]Assaingment3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,67 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-
+  
+  before_action :correct_user, only: [:edit, :update]
+  
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+    @user = @book.user
+    @books = Book.all
+    render:index
+    end
   end
 
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
+    flash[:notice] = "You have created book successfully."
   end
 
+
   def show
+    @books = Book.find(params[:id])  
+    @book = Book.new
+    @user = @books.user
   end
+  
+  
+  def edit
+     @book = Book.find(params[:id])  
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
+    redirect_to book_path(@book.id)  
+    else
+    render:edit
+    end
+  end
+  
+  def destroy
+   book = Book.find(params[:id])  
+   book.destroy 
+  redirect_to books_path
+  end
+  
   
   # 投稿データのストロングパラメータ
   private
 
   def book_params
     params.require(:book).permit(:title, :image, :body)
+  end
+  
+  
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
  
   devise_for :users
   root to: "homes#top"
-  get "/homes/about" => "homes#about", as: "about"
-  resources :books, only: [:new, :index, :show, :create]
-  resources :users, only: [:show, :index, :edit]
+  get "/home/about" => "homes#about", as: "about"
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
+  resources :users, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
